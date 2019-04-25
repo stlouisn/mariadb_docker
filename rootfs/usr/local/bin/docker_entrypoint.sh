b5c0972b2cf116ec1c354f0d0ab5baf8a4a449bc
stlouisn <stlouisn@hotmail.com>
2019-04-25 17:43:34 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -20,9 +20,6 @@ for arg; do
 done
 
 # usage: file_env VAR [DEFAULT]
-#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
-# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
-#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
 file_env() {
 	local var="$1"
 	local fileVar="${var}_FILE"
@@ -56,28 +53,16 @@ _check_config() {
 }
 
 # Fetch value from server config
-# We use mysqld --verbose --help instead of my_print_defaults because the
-# latter only show values present in config files, and not server defaults
 _get_config() {
 	local conf="$1"; shift
 	"$@" --verbose --help --log-bin-index="$(mktemp -u)" 2>/dev/null \
 		| awk '$1 == "'"$conf"'" && /^[^ \t]/ { sub(/^[^ \t]+[ \t]+/, ""); print; exit }'
-	# match "datadir      /some/path with/spaces in/it here" but not "--xyz=abc\n     datadir (xyz)"
 }
 
-## allow the container to be started with `--user`
-#if [ "$1" = 'mysqld' -a -z "$wantHelp" -a "$(id -u)" = '0' ]; then
-#	_check_config "$@"
-#	DATADIR="$(_get_config 'datadir' "$@")"
-#	mkdir -p "$DATADIR"
-#	find "$DATADIR" \! -user mysql -exec chown mysql '{}' +
-#	exec gosu mysql "$BASH_SOURCE" "$@"
-#fi
-
 if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
-	# Check config
+	# check config
 	_check_config "$@"
-	# Get config
+	# get config
 	DATADIR="$(_get_config 'datadir' "$@")"
 
 	if [ ! -d "$DATADIR/mysql" ]; then
@@ -93,12 +78,11 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 		echo 'Initializing database'
 		installArgs=( --datadir="$DATADIR" --rpm )
 		if { mysql_install_db --help || :; } | grep -q -- '--auth-root-authentication-method'; then
-			# beginning in 10.4.3, install_db uses "socket" which only allows system user root to connect, switch back to "normal" to allow mysql root without a password
-			# see https://github.com/MariaDB/server/commit/b9f3f06857ac6f9105dc65caae19782f09b47fb3
-			# (this flag doesn't exist in 10.0 and below)
+			# install_db uses "socket" which only allows system user root to connect
+			# switch back to "normal" to allow mysql root without a password
 			installArgs+=( --auth-root-authentication-method=normal )
 		fi
-		# "Other options are passed to mysqld." (so we pass all "mysqld" arguments directly here)
+		# "other options are passed to mysqld." (so we pass all "mysqld" arguments directly here)
 		mysql_install_db "${installArgs[@]}" "${@:2}"
 		echo 'Database initialized'
 
@@ -134,8 +118,6 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 		# default root to listen for connections from anywhere
 		file_env 'MYSQL_ROOT_HOST' '%'
 		if [ ! -z "$MYSQL_ROOT_HOST" -a "$MYSQL_ROOT_HOST" != 'localhost' ]; then
-			# no, we don't care if read finds a terminating character in this heredoc
-			# https://unix.stackexchange.com/questions/265149/why-is-set-o-errexit-breaking-this-read-heredoc-expression/265151#265151
 			read -r -d '' rootCreate <<-EOSQL || true
 				CREATE USER 'root'@'${MYSQL_ROOT_HOST}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 				GRANT ALL ON *.* TO 'root'@'${MYSQL_ROOT_HOST}' WITH GRANT OPTION ;
@@ -143,8 +125,6 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 		fi
 
 		"${mysql[@]}" <<-EOSQL
-			-- What's done in this file shouldn't be replicated
-			--  or products like mysql-fabric won't work
 			SET @@SESSION.SQL_LOG_BIN=0;
 
 			DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost') ;
@@ -197,5 +177,5 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 	fi
 fi
 
-# change to gosu mysql
-exec "$@"
+# Start mysql in interactive mode
+gosu mysql exec "$@"
